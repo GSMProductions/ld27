92cc2d793b927b1db90a16c1df432c10e35e21af
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-08-25 14:31:53 +0200

    hunting complete

--- sources/Assets/scripts/navMonster.cs
+++ sources/Assets/scripts/navMonster.cs
@@ -9,11 +9,20 @@ public class navMonster : MonoBehaviour {
 	// Use this for initialization
 
 	private List<Vector3> targets = new List<Vector3>();
-	private bool trackPlayer = false;
-
 	private int index=-1;
 	
+	public float searchRadius = 10f;
 
+	private int nSearch = 3;
+
+	private enum state
+		{
+		hunting,
+		warning,
+		walking
+		}
+
+	private state status = state.walking;
 	
 	void Start () 
 		{
@@ -26,28 +35,83 @@ public class navMonster : MonoBehaviour {
 		}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update() 
 		{
-		if(global.GetComponent<clock>().who == clock.turn.enemy)
+		
+		switch(status)
 			{
-			GetComponent<NavMeshAgent>().Resume();
-			if(trackPlayer)
+			
+			case state.walking :
+
+				if(playerInFieldVision())
+					{
+					GetComponent<NavMeshAgent>().destination= target.transform.position;
+					status = state.hunting;
+					}	
+				else
+					{
+					if(GetComponent<NavMeshAgent>().remainingDistance <= GetComponent<NavMeshAgent>().stoppingDistance)
+						{
+					    chooseTarget();
+						}
+					}
+				break;
+			case state.hunting:
+
+				if(playerInFieldVision())
+					{
+					GetComponent<NavMeshAgent>().destination= target.transform.position;
+					}
+				else
+					{
+					status = state.warning;
+					}
+				break;
+
+			case state.warning :
+
+				if(playerInFieldVision())
+					{
+					GetComponent<NavMeshAgent>().destination= target.transform.position;
+					status = state.hunting;
+					}	
+				else
+					{
+					if(GetComponent<NavMeshAgent>().remainingDistance <= GetComponent<NavMeshAgent>().stoppingDistance)
+						{
+						if(nSearch > 0)
+							{
+							GetComponent<NavMeshAgent>().destination =  getRandomPoint(transform.position,searchRadius);
+							}
+						else
+							{
+					    	status = state.walking;
+					    	}
+						}
+					}
+				break;
+			}
+
+			if(global.GetComponent<clock>().who == clock.turn.enemy)
 				{
-				GetComponent<NavMeshAgent>().destination= target.transform.position;
+				GetComponent<NavMeshAgent>().Resume();
 				}
 			else
 				{
-				if(GetComponent<NavMeshAgent>().remainingDistance <= GetComponent<NavMeshAgent>().stoppingDistance)
-					{
-					    chooseTarget();
-					}
+				GetComponent<NavMeshAgent>().Stop();
 				}
-			}
-		else
-			{
-			GetComponent<NavMeshAgent>().Stop();
-			}
-	
+			
+		}
+
+	private Vector3 getRandomPoint(Vector3 origin, float reach)
+		{
+		float dx = Random.Range(-reach,reach);
+		float dy = Random.Range(-reach,reach);
+
+		origin.x += dx;
+		origin.y += dy;
+
+		return origin;
 		}
 
 	private void chooseTarget()
@@ -59,5 +123,49 @@ public class navMonster : MonoBehaviour {
 
 		index = newindex;
 		GetComponent<NavMeshAgent>().destination= targets[index];
+		}
+
+	private bool playerInFieldVision()
+		{
+		Vector3 direction = transform.forward;
+
+		Vector3 targetPosition = target.transform.position;
+		Vector3 vectorTarget = new Vector3(targetPosition.x-transform.position.x, targetPosition.y-transform.position.y, targetPosition.z-transform.position.z);
+
+
+		float scalarProduct = direction.x * vectorTarget.x +  direction.y * vectorTarget.y +  direction.z * vectorTarget.z;
+
+		if (scalarProduct < 0f)
+			{
+			return false;
+			}
+
+		Ray ray = new Ray(transform.position,transform.forward);
+		RaycastHit hit;
+
+		Color color = Color.clear;
+
+		if (Physics.Raycast(transform.position,vectorTarget.normalized, out hit))
+			{
+			color = Color.green;
+			if (hit.collider==target.collider)
+				{
+				color = Color.blue;
+				float radius = searchRadius;
+				if(status != state.walking)
+					{
+					radius *= 1.5f;
+					}
+
+				if(hit.distance <= radius)
+					{
+					color = Color.red;
+					return true;
+					}
+				}
+
+	   	 	Debug.DrawLine(ray.origin, hit.transform.position,color);
+	   	 	}
+		return false;
 		}
 }
