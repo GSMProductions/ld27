5ef1d61929c04a7282c4eef2f7fe503b3007b528
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-08-26 04:31:54 +0200

    Cleaning up the mess

---
+++ sources/Assets/scripts/flicker.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ public class flicker : MonoBehaviour {
+   
+    float timer;
+    float wait;
+    public bool flickering = false;
+ 
+    void Start() {
+    	wait = Random.Range(0f,3f);
+   	}
+
+    void Update(){
+       timer +=Time.deltaTime;
+       if(timer>wait && flickering){
+         Blink();
+         timer =0f;
+         wait = Random.Range(0f,3f);
+       }
+    }
+
+    void Blink(){
+    	light.enabled = !light.enabled;
+    }
+}
