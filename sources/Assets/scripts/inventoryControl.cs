28609b7c7375504af09f53b93c801086d05b4029
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-08-26 11:46:38 +0200

    commit for merge

---
+++ sources/Assets/scripts/inventoryControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inventoryControl : MonoBehaviour 
+	{
+
+	// Use this for initialization
+	void Start () {
+		guiTexture.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Debug.Log("enabled --> " + guiTexture.enabled);
+		if(guiTexture.enabled)
+			{
+ 		
+			if (Input.GetKeyDown(KeyCode.E)||Input.GetKeyDown(KeyCode.Escape)) 
+				{
+			    // hide
+			   Debug.Log("False");
+			   guiTexture.enabled = false;
+				}
+			}
+		else
+			{
+			if (Input.GetKeyDown(KeyCode.E)) 
+				{
+    			Debug.Log("True");
+    			guiTexture.enabled = true;
+				}
+			}
+		}
+		
+	}
+
