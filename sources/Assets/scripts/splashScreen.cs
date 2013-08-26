7040a3b4a5fd5d53bdb699ff9c3e923bc6a93f9c
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-08-26 16:17:49 +0200

    Splash Screen implemented

---
+++ sources/Assets/scripts/splashScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splashScreen : MonoBehaviour {
+
+	public float timer = 2f;
+	public string levelToLoad = "Level";
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("DisplayScene");
+	}
+	
+
+	IEnumerator DisplayScene() {
+		yield return new WaitForSeconds(timer);
+		Application.LoadLevel(levelToLoad);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
