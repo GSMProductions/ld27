1868dd7ae73a334581bebd2a1acfc38e1df702b4
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-08-26 10:57:43 +0200

    Restarting the intro

---
+++ sources/Assets/scripts/doorOpenable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorOpenable : MonoBehaviour {
+
+	public bool unlocked = true;
+	public enum doorStates {Closed, Closing, Open, Opening};
+	public doorStates currentState = doorStates.Opening;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentState == doorStates.Opening) {
+			transform.Rotate(0,0,Time.deltaTime*50);
+		}
+	
+	}
+}
--- sources/Assets/scripts/flipCard.cs
+++ sources/Assets/scripts/flipCard.cs
@@ -5,22 +5,33 @@ public class flipCard : MonoBehaviour {
 
 	public bool hasFlipped = false;
 	public bool isFlipping = false;
+	public bool activeAtStart = false;
+	public GameObject nextCard = null;
 
 	// Use this for initialization
 	void Start () {
-	
+		gameObject.SetActive(activeAtStart);
 	}
 	
 	// Update is called once per frame
-	// Update is called once per frame
 	void Update () {
-		
 		if (isFlipping) {
 			transform.Rotate(Time.deltaTime*100, 0, 0);
 		}
 	}
 
+
+	void OnMouseDown() {
+		Flip();
+	}
+
+
 	void Flip() {
+		if (nextCard != null) {
+			nextCard.SetActive(true);
+			audio.Stop();
+			nextCard.audio.Play();
+		}
 		if (!hasFlipped) {
 			isFlipping = true;
 		}
