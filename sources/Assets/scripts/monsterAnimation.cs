3e4a6f1f81f1fdd5bc729460ba1f36429c7da21e
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-08-26 15:51:03 +0200

    Monster prefab

---
+++ sources/Assets/scripts/monsterAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterAnimation : MonoBehaviour {
+
+
+	protected Animator animator;
+	public clock globals;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (animator) {
+			if (globals.who == clock.turn.enemy) {
+				animator.SetFloat("Speed", 2f);
+			}
+			else {
+				animator.SetFloat("Speed",0f);
+			}
+		}
+
+	}
+}
