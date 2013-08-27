17e4a2eb0062aad297b6947e0fcfc8cc9d189657
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-08-27 03:18:25 +0200

    Outro scene

---
+++ sources/Assets/scripts/outro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class outro : MonoBehaviour {
+	float timeSinceStart;
+
+	// Use this for initialization
+	void Start () {
+		timeSinceStart = 0f;
+		CameraFade.StartAlphaFade(Color.black, true, 3f, 17f, () => { } );
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceStart += Time.deltaTime;
+
+		if (timeSinceStart > 28f) {
+			Application.LoadLevel("MainMenu");
+		}
+
+	}
+}
